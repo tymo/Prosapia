6aa18e899a74571d2a38632e59393199a5fb3479
Rafael Maus <rfmaus@hotmail.com>
2019-02-18 10:43:14 -0300

    Refactored FormElement.js and InputBuilder.js to create form elements using document.createElement function

--- public_html/js/component/element/FormElement.js
+++ public_html/js/component/element/FormElement.js
@@ -5,46 +5,13 @@ angular.module("prosapia").factory('FormElement', function () {
         DATEPICKER: 'DTP',
         SELECT: 'SLT',
         BUTTONSUBMIT: 'BTN',
-//        elements: [
-//            {type: 'TXT', content: '<input type="text" name="<name>" ng-model="data.<model>" placeholder="<placeholder>" class="textInput"/>'},
-//            {type: 'TXD', content: '<input type="text" ng-model="data.<model>" name="<name>" placeholder="<placeholder>" ui-date/>'},
-//            {type: 'DTP', content: '<input type="date" name="<name>" ng-model="data.<model>" placeholder="<placeholder>" class="textInput" />'},
-//            {type: 'BTN', content: '<button class="addButton" name="sendButton" ng-click="addItem(listName, data)">Adicionar</button>'},
-//            {type: 'SLT', content: '<select class="form-control" ng-model="data.<model>" ng-options="<options>">\
-//             <option value=""><option></option>\
-//             </select>'}
-//        ],
+
         elements: [
             {type: 'TXT', tag: 'INPUT'},
-//            {type: 'TXD', tag: '<input type="text" ng-model="data.<model>" name="<name>" placeholder="<placeholder>" ui-date/>'},
-//            {type: 'DTP', tag: '<input type="date" name="<name>" ng-model="data.<model>" placeholder="<placeholder>" class="textInput" />'},
             {type: 'BTN', tag: 'BUTTON'},
             {type: 'SLT', tag: 'SELECT'},
             {type: 'OPT', tag: 'OPTION'}
-//            {type: 'SLT', tag: '<select class="form-control" ng-model="data.<model>" ng-options="<options>">\
-//             <option value=""><option></option>\
-//             </select>'}
         ],
-//        getElement: function (element) {
-//            let tplt = (this.elements.filter(function (cp) {
-//                return cp.type === element.type;
-//            })[0] ? this.elements.filter(function (cp) {
-//                return cp.type === element.type;
-//            })[0].content : null);
-//            if (tplt) {
-//                if (element.type === this.BUTTONSUBMIT) {
-//                    tplt = tplt.replace('<insertListemer>', element.listener).replace('<dataObject>', element.dataObject);
-//                } else if (element.type === this.SELECT) {
-//                    tplt = tplt.replace('<model>', element.model).replace('<options>', element.options).replace('<option>', element.option);
-//                } else if ([this.TEXTINPUT, this.DATEINPUT, this.DATEPICKER].includes(element.type)) {
-//                    tplt = tplt.replace('<name>', element.name).replace('<model>', element.model).replace('<placeholder>', element.placeholder);
-//                    tplt += "<br>";
-//                }
-//            } else {
-//                console.warn("Component type \'" + element.type + "\' is not defined.");
-//            }
-//            return tplt;
-//        }
         getElement: function (element) {
             let tag = (this.elements.filter(function (cp) {
                 return cp.type === element.type;
@@ -59,10 +26,24 @@ angular.module("prosapia").factory('FormElement', function () {
                 }
                 if (element.id) {
                     newElement.id = element.id;
-                }               
-//                if (element.placeholder) {
-//                    newElement.placeHolder = element.placeholder;
-//                }
+                }
+                if (element.placeHolder) {
+                    newElement.setAttribute('placeHolder', element.placeHolder);
+                }
+                if (element.click) {
+                    newElement.setAttribute("ng-click", element.click);
+                }
+                if (element.options) {
+                    newElement.setAttribute("ng-options", element.options);
+                    opt = document.createElement("OPTION");
+                    opt.appendChild(document.createTextNode(element.option));
+                    opt.setAttribute("value", "");
+                    opt.setAttribute("label", element.option);
+                    newElement.appendChild(opt);
+                }
+                if (element.model) {
+                    newElement.setAttribute("ng-model", "data." + element.model);
+                }
                 if (element.eType) {
                     newElement.type = element.eType;
                 }
--- public_html/js/component/inputBuilder.js
+++ public_html/js/component/inputBuilder.js
@@ -21,56 +21,13 @@ angular.module("prosapia").directive('inputBuilder', function ($compile, FormEle
             newElement = FormElement.getElement(input);
             if (newElement) {
                 newForm.appendChild(newElement);
+                newForm.appendChild(document.createElement("BR"));
             }
         });
         scope.List = List;
-//        document.body.appendChild(newForm);
-        if (scope.listName === "medicineList") {
-            document.getElementById("mdcInput").appendChild(newForm);
-        }
-        if (scope.listName === "dosageList") {
-            document.getElementById("dsgInput").appendChild(newForm);
-        }
-        //**
+        $compile(newForm)(scope);
+        document.getElementById(scope.listName).appendChild(newForm);
         scope.inputFields.forEach(function (input) {
-            if (input.type === FormElement.BUTTONSUBMIT) {
-                let btn = document.getElementById(input.id);
-                if (btn) {
-                    btn.setAttribute("ng-click", input.click);
-                    $compile(btn)(scope);
-                }
-            }
-            if (input.type === FormElement.TEXTINPUT) {
-                let edt = document.getElementById(input.id);
-                if (edt) {
-                    edt.setAttribute("ng-model", "data." + input.model);
-                    edt.setAttribute("placeHolder", input.placeHolder);
-                    $compile(edt)(scope);
-                }
-            }
-            if (input.type === FormElement.SELECT) {
-                let slt = document.getElementById(input.id);
-                if (slt) {
-                    slt.setAttribute("ng-model", "data." + input.model);
-                    slt.setAttribute("ng-options", input.options);
-                    opt = document.createElement("OPTION");
-                    opt.appendChild(document.createTextNode(input.option));
-                    opt.setAttribute("value", "");
-                    opt.setAttribute("label", input.option);
-                    slt.appendChild(opt);
-                    $compile(slt)(scope);
-                }
-            }
         })
-
-
-//        var el = angular.element("div_id");
-//        $scope = el.scope();
-//        $injector = el.injector();
-//        $injector.invoke(function ($compile) {
-//            $compile(el)($scope)
-//        })
-        //**
-
     }
 });
